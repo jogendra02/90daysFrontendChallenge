683e5e96e4185960a62d9f1361073df2eed9ec7b
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-06 22:10:05 +0530

    script3.js file is updated

---
+++ DAY1/script3.js
@@ -0,0 +1,36 @@
+const product1Name = "Nykaa Matte Lipstick";
+let product1Price = 599;
+const product1Rating = 4.5;
+const product1InStock = true;
+let product1DiscountPercent = 15;
+
+const product2Name = "Nykaa Skin Serum";
+let product2Price = 1299;
+const product2Rating = 4.7;
+const product2InStock = true;
+let product2DiscountPercent = 20;
+
+
+const product3Name = "Nykaa Kajal";
+let product3Price = 299;
+const product3Rating = 4.3;
+const product3InStock = false;
+let product3DiscountPercent = 10;
+
+
+const product4Name = "Nykaa Face Wash";
+let product4Price = 449;
+const product4Rating = 4.0;
+const product4InStock = true;
+let product4DiscountPercent = 12;
+
+const product5Name = "Nykaa Nail Polish";
+let product5Price = 199;
+const product5Rating = 3.9;
+const product5InStock = true;
+let product5DiscountPercent = 8;
+console.log(product4Name)
+console.log(product3Rating)
+console.log(product1DiscountPercent)
+console.log(product1DiscountPercent)
+console.log(product2Price)
