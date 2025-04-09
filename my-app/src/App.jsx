93c6bc505538c5cc69923fdbc0f2a6e1fc04082d
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-09 22:30:10 +0530

    index.html and app.jsx files are updated

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,19 +8,30 @@ function App() {
 
   return (
     <>
+    <div className="container">
+      <div className="row">
+        <div class="col-6">
+          <p>Instagram photos</p>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-6">
+
+        
       <h3>Instagram</h3>
-      <div>
+      
         <label for ="emailtext">Email</label>
-        <input type = "text"></input><br></br><br></br>
+        <input type = "text" placeholder='Username'></input><br></br><br></br>
         <label for ="passwordtext">Password
 
         </label>
-        <input type = "text"></input><br></br><br></br><br></br>
+        <input type = "text" placeholder='password'></input><br></br><br></br><br></br>
                 <button type='text'>Login</button>
         
 
-        
-        </div>    
+        </div>
+        </div> 
+        </div>   
               </>
   )
 }
