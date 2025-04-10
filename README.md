4f127e5a5479eda2eb619b458d16b2042b9e359e
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-10 22:19:55 +0530

    index5.html file is updated

--- README.md
+++
@@ -1,3 +0,0 @@
-# 90daysFrontendChallenge
-90 days challange to become an industry ready.
-
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,31 +8,27 @@ function App() {
 
   return (
     <>
-    <div className="container">
-      <div className="row">
-        <div class="col-6">
-          <p>Instagram photos</p>
-        </div>
-        </div>
-        <div class="row">
-        <div class="col-6">
-
-        
-      <h3>Instagram</h3>
-      
-        <label for ="emailtext">Email</label>
-        <input type = "text" placeholder='Username'></input><br></br><br></br>
-        <label for ="passwordtext">Password
-
-        </label>
-        <input type = "text" placeholder='password'></input><br></br><br></br><br></br>
-                <button type='text'>Login</button>
-        
-
-        </div>
-        </div> 
-        </div>   
-              </>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
