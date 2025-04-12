390394c03d2d9f5dbb9ac26c04f326013bad45bb
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-12 13:01:15 +0530

    script4.js file is updated

---
+++ DAY1/script4.js
@@ -0,0 +1,21 @@
+const obj = {
+    name :"Naveen",
+    age :"22",
+    location :"tirupathi"
+};
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.location);
+
+
+const {name, age, location} = obj;
+console.log(obj);
+
+const array = [
+    name ,"Jogendra kumari",
+    age ,"22",
+    location ,"machilipatnam"
+
+
+];
+console.log(array);
