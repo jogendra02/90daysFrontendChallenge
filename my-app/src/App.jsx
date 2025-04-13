7c95b19521229f101cb2d34dd1aa4b6eb97249fa
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-13 22:48:11 +0530

    style4.css file is updated

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './Components/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <h2>Home page</h2>
+      <Login></Login>
     </>
-  )
+  );
 }
 
 export default App
