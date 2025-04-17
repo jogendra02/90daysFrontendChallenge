643368afa67b608da4542565520c9b1e56fdb04f
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-17 22:09:08 +0530

    script2.js file is updated

--- DAY1/script2.js
+++ DAY1/script2.js
@@ -2,5 +2,5 @@ const mobile =["vivo", "realme", "oppo", "redme", "iphone"];
 console.log(mobile[3]);
 const car =["toyota", "saab", "bmw"];
 console.log(car[2]);
-const color =["blue", "red", "pink", "yellow", "orange"];
+const color =["blue", "red", "pink", "yellow", "orange", "white"];
 console.log(color[2]);
