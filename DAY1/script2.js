3031c6ade277e4448046e3f28c697d39ea2613f5
jogendra02 <jogendrakumarich207@gmail.com>
2025-04-18 21:17:54 +0530

    style.css file is updated

--- DAY1/script2.js
+++ DAY1/script2.js
@@ -1,6 +1,6 @@
 const mobile =["vivo", "realme", "oppo", "redme", "iphone"];
 console.log(mobile[3]);
-const car =["toyota", "saab", "bmw"];
+const car =["toyota", "saab", "bmw","tata"];
 console.log(car[2]);
 const color =["blue", "red", "pink", "yellow", "orange", "white"];
 console.log(color[2]);
